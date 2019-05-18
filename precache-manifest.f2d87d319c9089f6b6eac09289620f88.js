8cbc1a554e605a3256ecab20a5e5047974d11aa5
Eduard Benedic <benediceduard@outlook.com>
2019-05-18 08:48:48 +0100

    Updates

---
+++ precache-manifest.f2d87d319c9089f6b6eac09289620f88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1eff23254e712283a7abe8d4b5d2aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "66869eea7d97e9c62ddb",
+    "url": "/static/css/main.296995ef.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "66869eea7d97e9c62ddb",
+    "url": "/static/js/main.6ee60015.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
